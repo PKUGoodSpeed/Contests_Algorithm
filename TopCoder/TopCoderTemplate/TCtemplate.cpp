4f16d981258aa2d221da5402b6af5b640b0cbe1d
PKUGoodSpeed <pkulizebo@gmail.com>
2017-04-01 08:30:05 -0500

    add first stuffs

---
+++ TopCoder/TopCoderTemplate/TCtemplate.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+#define REP(i,s,n) for(int (i)=s; (i)<(int)(n);(i)++)
+#define RIT(it,c) for(__typeof(c.begin()) it = c.begin();it!=c.end();it++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (int)(x).size()
+#define MSET(m,v) memset(m,v,sizeof(m))
+
+/* define class and method names */
+#define TCC TopCoderClass
+#define TCM TopCoderMethod
+
+
+using namespace std;
+
+
+typedef long long LL;
+typedef long double LD;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<LL> vL;
+typedef vector<bool> vb;
+
+class TCC{
+public:
+	int TCM(vector <int> a, int K){
+		return 0;
+	}
+};
+
+
+/* testing process */
+int main(){
+	TCC test;
+	auto ans = TCC.TCM(vi(),10);
+	
+	/* output results */
+	cout<<ans;
+}
